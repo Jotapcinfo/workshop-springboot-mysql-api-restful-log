50cce0295dbc6972a237cfadfd40708e037da601
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-08-27 13:19:01 -0300

    Projeto criado

---
+++ src/main/java/br/com/portaljmti/log/PortaljmtiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.portaljmti.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaljmtiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaljmtiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portaljmti/log/PortaljmtiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portaljmti.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaljmtiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
