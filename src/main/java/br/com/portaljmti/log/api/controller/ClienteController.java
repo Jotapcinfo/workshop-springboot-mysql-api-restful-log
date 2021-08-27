91976b00933b8e0e391c9a3deedafb160cecaa17
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-08-27 16:19:26 -0300

    Implementação de CRUD Verbo GET usando method reference

--- src/main/java/br/com/portaljmti/log/api/controller/ClienteController.java
+++ src/main/java/br/com/portaljmti/log/api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.portaljmti.log.domain.model.Cliente;
@@ -14,20 +15,21 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List <Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable long clienteId) {
 		
 		return clienteRepository.findById(clienteId)
-				.map(cliente -> ResponseEntity.ok(cliente))
+				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
 	}
 }
