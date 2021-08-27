19d3b2b718799d8803e79b4c90b62b38b8ed723d
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-08-27 14:13:19 -0300

    Persistencia configuração com MySQL e Flyway

--- src/main/java/br/com/portaljmti/log/api/controller/ClienteController.java
+++ src/main/java/br/com/portaljmti/log/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.portaljmti.log.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,12 @@ import br.com.portaljmti.log.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List <Cliente> listar() {
-		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("11 99999-8888");
-		cliente1.setEmail("joaosilva@pjmti.com");	
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("José");
-		cliente2.setTelefone("11 99999-7777");
-		cliente2.setEmail("josesilva@pjmti.com");	
-        
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
