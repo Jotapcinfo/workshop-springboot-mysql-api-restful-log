6156b951bf7196beb74a570effac136270da6b63
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-08-27 15:53:10 -0300

    Implementação de CRUD Verbo GET usando repository Optional

--- src/main/java/br/com/portaljmti/log/api/controller/ClienteController.java
+++ src/main/java/br/com/portaljmti/log/api/controller/ClienteController.java
@@ -1,13 +1,17 @@
 package br.com.portaljmti.log.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.portaljmti.log.domain.model.Cliente;
 import br.com.portaljmti.log.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+
 
 @AllArgsConstructor
 @RestController
@@ -18,5 +22,17 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List <Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable long clienteId) {
+		
+		Optional <Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		    return ResponseEntity.notFound().build();
+		
 	}
 }
