d5a114ef1ed50fe84eecb9cb682929e28325276d
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-08-30 08:18:21 -0300

    Validação em cascata e Validation Groups

---
+++ src/main/java/br/com/portaljmti/log/domain/ValidationGroups.java
@@ -0,0 +1,6 @@
+package br.com.portaljmti.log.domain;
+
+public interface ValidationGroups {
+	
+	public interface ClienteId { }
+}
--- src/main/java/br/com/portaljmti/log/domain/model/Entrega.java
+++ src/main/java/br/com/portaljmti/log/domain/model/Entrega.java
@@ -11,10 +11,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import br.com.portaljmti.log.domain.ValidationGroups;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,12 +34,18 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long Id;
 	
+	@Valid
+	@ConvertGroup(from = javax.validation.groups.Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
+	@NotNull
 	private BigDecimal taxa;
 	
 	@JsonProperty(access = Access.READ_ONLY)
