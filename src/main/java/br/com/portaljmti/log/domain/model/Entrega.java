0760d5ba5f03e9c2885065f72b5f3c094c7edb2f
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-09-01 19:31:38 -0300

    Simplificando a transformação de objetos com ModelMapper

--- src/main/java/br/com/portaljmti/log/domain/model/Entrega.java
+++ src/main/java/br/com/portaljmti/log/domain/model/Entrega.java
@@ -11,14 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
-import br.com.portaljmti.log.domain.ValidationGroups;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,27 +27,20 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long Id;
 	
-	@Valid
-	@ConvertGroup(from = javax.validation.groups.Default.class, to = ValidationGroups.ClienteId.class)
-	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
-	@Valid
-	@NotNull
+	
+	
 	@Embedded
 	private Destinatario destinatario;
 	
-	@NotNull
 	private BigDecimal taxa;
 	
-	@JsonProperty(access = Access.READ_ONLY)
 	@Enumerated(EnumType.STRING)
 	private StatusEntrega status;
 	
-	@JsonProperty(access = Access.READ_ONLY)
 	private OffsetDateTime dataPedido;
 	
-	@JsonProperty(access = Access.READ_ONLY)
 	private OffsetDateTime dataFinalizacao;
 }
